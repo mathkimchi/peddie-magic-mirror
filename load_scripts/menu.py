a364a0ebf11a2161f30673baf372bd4b06ae0349
mathkimchi <kingalvinjkim@gmail.com>
2024-09-10 15:09:07 -0400

    save menu to json

--- load_scripts/menu.py
+++ load_scripts/menu.py
@@ -1,5 +1,6 @@
 import requests
 import icalendar
+import json
 
 url = "https://peddie.org/events/month/?tribe_eventcategory%5B0%5D=250&ical=1"
 response = requests.get(url)
@@ -12,16 +13,18 @@ with open('data/menu.ics', 'wb') as file:
 with open('data/menu.ics', 'r') as file:
     calendar = icalendar.Calendar.from_ical(file.read())
 
-text = ""
-with open('data/menu.txt','w') as file:
+menu_events = []
+with open('data/menu.json','w') as file:
     for (index, event) in enumerate(calendar.walk('VEVENT')):
-        text += event.get("SUMMARY") + "\n"
-        text += event.get("DESCRIPTION") + "\n"
+        menu_event = {}
+        menu_event["meal_type"] = event.get("SUMMARY")
+        menu_event["foods"] = event.get("DESCRIPTION")
+        menu_events.append(menu_event)
 
         # break if this is last with redundant safety
         if (event.get("SUMMARY")=="Dinner") or (event.get("SUMMARY")=="Family Style Dinner") or (index>=2):
             break
-    file.write(text)
+    file.write(json.dumps(menu_events, indent=4))
 
-f = open("data/menu.txt", "r")
+f = open("data/menu.json", "r")
 print(f.read())
