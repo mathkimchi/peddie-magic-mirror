e24d5d50afa7f077a940a5aaa3d5f8e73918606e
jaden <jliu-29@peddie.org>
2026-09-05 13:52:07 -0400

    more weather stuff

--- load_scripts/weather.py
+++ load_scripts/weather.py
@@ -19,12 +19,45 @@ def get_weather_data(url):
     info["weather_code"] = current["weatherCode"]
     return info
 
+def get_weather_icon(code):
+    weather_icons = {
+    "113": "☀️",  # Sunny
+    "116": "🌤️",  # Partly cloudy
+    "119": "☁️",  # Cloudy
+    "122": "☁️",  # Overcast
+    "176": "🌦️",  # Patchy rain
+    "200": "⛈️",  # Thunder
+    "227": "🌨️",  # Blowing snow
+    "248": "🌫️",  # Fog
+    "260": "🌫️",  # Freezing fog
+    "263": "🌦️",  # Patchy drizzle
+    "266": "🌧️",  # Light drizzle
+    "293": "🌦️",  # Light rain
+    "296": "🌧️",  # Light rain
+    "299": "🌧️",  # Moderate rain
+    "302": "🌧️",  # Moderate rain
+    "308": "🌧️",  # Heavy rain
+    "311": "🌧️",  # Light freezing rain
+    "320": "🌨️",  # Light sleet
+    "326": "🌨️",  # Light snow
+    "335": "❄️",  # Heavy snow
+    "338": "❄️",  # Heavy snow
+    "353": "🌦️",  # Light rain shower
+    "356": "🌧️",  # Moderate rain shower
+    "368": "🌨️",  # Light snow shower
+    "386": "⛈️",  # Thundery shower
+    "395": "⛈️",  # Heavy snow shower
+    }
+    icon = weather_icons.get(code, "❓")
+    return icon
+
+
 if __name__ == "__main__":
     URL = "https://wttr.in/?format=j1"
     weather = get_weather_data(URL)
-    with open("weather.txt", "w") as file:
-        file.write(f"{weather['temperature']}°F\n")
-        file.write(f"Wind: {weather['wind']}mph\n")
-        file.write(f"{weather['condition']}\n")
-        file.write(f"{weather['weather_code']}")
-    print(weather)
+    file.write(f"{weather['temperature']}°F\n")
+    file.write(f"Wind: {weather['wind']}mph\n")
+    file.write(f"{weather['condition']}\n")
+    icon = get_weather_icon(f"{weather['weather_code']}")
+    file.write(icon)
+    print(__file__)
